719e66e38c13262e11c0ae95928665439217c7cc
shalauddinahmedshipon <sshimi910@gmail.com>
2025-09-18 12:23:46 +0600

    dev branch created

--- src/swagger/swagger.config.ts
+++ src/swagger/swagger.config.ts
@@ -1,7 +1,7 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerConfig = new DocumentBuilder()
-  .setTitle('Fahad Multi-vendor API Documentation')
+  .setTitle('mkasraoui-server API Documentation')
   .setDescription('Comprehensive API documentation for the application services')
   .setVersion('1.0')
   .addCookieAuth('refreshToken')
