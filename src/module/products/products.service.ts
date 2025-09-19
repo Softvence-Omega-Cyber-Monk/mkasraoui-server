a01c1d5104348553b5499954e35553c2444779a5
linux44gh6 <abireshan32@gmail.com>
2025-09-19 10:47:28 +0600

    update code

--- src/module/products/products.service.ts
+++ src/module/products/products.service.ts
@@ -1,6 +1,4 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-
-
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductDTO } from './dto/create-product.dto';
@@ -11,34 +9,35 @@ export class ProductsService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createProductDto: CreateProductDTO, imges: Express.Multer.File[]) {
-   try{
-     const imagePaths = imges?.map((file) => buildFileUrl(file.filename)) || [];
-    
-    const res=await this.prisma.product.create({
-      data: {
-        title: createProductDto.title,
-        description: createProductDto.description,
-        product_type: createProductDto.product_type,
-        age_range: createProductDto.age_range,
-        rattings: createProductDto.rattings,
-        review: createProductDto.review,
-        price: createProductDto.price,
-        included: createProductDto.included,
-        tutorial: createProductDto.tutorial,
-        imges: imagePaths, 
-        activities: {
-          create: createProductDto.activities.map((a) => ({
-            title: a.title,
-            description: a.description,
-          })),
+    try {
+      const imagePaths = imges?.map((file) => buildFileUrl(file.filename)) || [];
+
+      const res = await this.prisma.product.create({
+        data: {
+          title: createProductDto.title,
+          description: createProductDto.description,
+          product_type: createProductDto.product_type,
+          age_range: createProductDto.age_range,
+          rattings: createProductDto.rattings,
+          review: createProductDto.review,
+          price: createProductDto.price,
+          included: createProductDto.included,
+          tutorial: createProductDto.tutorial,
+          imges: imagePaths,
+          activities: {
+            create: createProductDto.activities.map((a) => ({
+              title: a.title,
+              description: a.description,
+            })),
+          },
         },
-      },
-      include: { activities: true },
-    });
-    return res
-   }catch(err){
-    console.log(err)
-   }
+        include: { activities: true },
+      });
+      return res;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
   }
 
   async findAll() {
@@ -58,31 +57,51 @@ export class ProductsService {
     return product;
   }
 
-  async update(id: string, updateProductDto: UpdateProductDto, imges?: Express.Multer.File[]) {
-    const imagePaths = imges?.map((file) => file.filename);
-    // const { activities, imges: _, ...productData } = updateProductDto;
+  async update(id: string, updateProductDto: UpdateProductDto, images?: Express.Multer.File[]) {
+    try {
+      const { activities, ...productData } = updateProductDto;
+      const imagePaths = images?.map((file) => buildFileUrl(file.filename));
 
-    // return this.prisma.product.update({
-    //   where: { id },
-    //   data: {
-    //     ...productData,
-    //     ...(imagePaths ? { imges: imagePaths } : {}),
-    //     activities: activities
-    //       ? {
-    //           deleteMany: { productId: id }, 
-    //           create: activities.map((a) => ({
-    //             title: a.title,
-    //             description: a.description,
-    //           })),
-    //         }
-    //       : undefined,
-    //   },
-    //   include: { activities: true },
-    // });
+      return this.prisma.$transaction(async (prisma) => {
+        if (activities) {
+          await prisma.activity.deleteMany({
+            where: { productId: id },
+          });
+        }
+
+        const updatedProduct = await prisma.product.update({
+          where: { id },
+          data: {
+            ...productData,
+            ...(imagePaths ? { imges: imagePaths } : {}),
+          },
+        });
+
+        if (activities && activities.length > 0) {
+          await prisma.activity.createMany({
+            data: activities.map((a) => ({
+              title: a.title,
+              description: a.description,
+              productId: updatedProduct.id,
+            })),
+          });
+        }
+
+        return prisma.product.findUnique({
+          where: { id },
+          include: { activities: true },
+        });
+      });
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
   }
 
   async remove(id: string) {
-    await this.prisma.activity.deleteMany({ where: { productId: id } });
-    return this.prisma.product.delete({ where: { id } });
+    return this.prisma.$transaction(async (prisma) => {
+      await prisma.activity.deleteMany({ where: { productId: id } });
+      return prisma.product.delete({ where: { id } });
+    });
   }
 }
