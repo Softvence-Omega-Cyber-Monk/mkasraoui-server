a59df2f98726b6f3646d40a514c385470e6efa66
shalauddinahmedshipon <sshimi910@gmail.com>
2025-09-27 15:17:44 +0600

    added

--- src/module/mail/mail.module.ts
+++ src/module/mail/mail.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
+import { MailTemplatesService } from './invitationFormat';
 
 @Module({
   controllers: [MailController],
-  providers: [MailService,],
+  providers: [MailService,MailTemplatesService],
 })
 export class MailModule {}
