b2ea5aece4b7182e64d5c9dcb983ace4a174d729
linux44gh6 <abireshan32@gmail.com>
2025-09-28 10:56:36 +0600

    udpate data

--- src/module/invitations/dto/create-invitation.dto.ts
+++ src/module/invitations/dto/create-invitation.dto.ts
@@ -1,8 +1,24 @@
-
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsUrl } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger'; // 👈 Import ApiProperty
 
 export class CreateInvitationDto {
+  @ApiProperty({
+    description: 'The email address of the user being invited.',
+    example: 'alice.smith@example.com',
+    type: String, // Optional, but good practice
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
+
+  @ApiProperty({
+    description: 'A publicly accessible URL pointing to the user’s profile picture.',
+    example: 'https://cdn.example.com/profiles/alice.jpg',
+    type: String, // Optional, but good practice
+  })
+  @IsUrl(
+    { require_protocol: true },
+  )
+  @IsNotEmpty()
+  imageUrl: string;
 }
